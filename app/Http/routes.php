22d6b74147982cddbd84687a0e802bae8d7499a8
ChaosBelcebub <mich3ael@web.de>
2016-03-18 11:08:44 +0100

    Try to enable login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController'
+]);
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
