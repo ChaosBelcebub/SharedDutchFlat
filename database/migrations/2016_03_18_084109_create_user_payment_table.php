0830309999989c859615107731169f64c8bfc1e6
vagrant <vagrant@homestead>
2016-03-18 08:53:43 +0000

    Fixed migration. Update composer and disable previous installed package GeneratorServiceProvider

--- database/migrations/2016_03_18_084109_create_user_payment_table.php
+++ database/migrations/2016_03_18_084109_create_user_payment_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePaymentUserPivotTable extends Migration
+class CreateUserPaymentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,9 +14,9 @@ class CreatePaymentUserPivotTable extends Migration
     {
         Schema::create('payment_user', function (Blueprint $table) {
             $table->integer('payment_id')->unsigned()->index();
-            $table->foreign('payment_id')->references('id')->on('payments')->onDelete('cascade');
+            $table->foreign('payment_id')->references('id')->on('payments');
             $table->integer('user_id')->unsigned()->index();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->primary(['payment_id', 'user_id']);
         });
     }
